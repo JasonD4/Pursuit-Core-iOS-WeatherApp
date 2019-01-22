4bef2ac5439486e389f27424dafb9e5e9beb81c0
Jason <Jason@Pursuits-MacBook-Pro-42.local>
2019-01-22 14:21:38 -0500

    Weather model done

--- WeatherApp/WeatherApp/Models/WeatherModel.swift
+++ WeatherApp/WeatherApp/Models/WeatherModel.swift
@@ -10,3 +10,22 @@ import Foundation
 
 //4u5xzN4BsqrY333a2SfCJ
 //9KrlszKWUe6QXQfBMDD2OAckfUoYt8YQwqXCmISN
+struct Weather: Codable {
+    let response: [Days]
+}
+
+struct Days: Codable {
+    let periods: [DayAndWeather]
+}
+
+struct DayAndWeather: Codable {
+    let validTime: String
+    let maxTempF: Int
+    let minTempF: Int
+    let precipIN: Int
+    let weather: String
+    let icon: String
+    let sunrise: Int
+    let sunset: Int
+    let windGustMPH: Int
+}
